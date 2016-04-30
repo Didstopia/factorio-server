03e1ff48e444d345e9313260d7e7330e1ac43ce3
Pauli Jokela <pauli.jokela@didstopia.com>
2016-04-30 15:33:36 +0300

    Added autosave restore support

--- start_factorio.sh
+++ start_factorio.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
 
-pid=0
-
-trap 'exit_handler' SIGHUP SIGINT SIGQUIT SIGTERM
-exit_handler()
-{
-	echo "Shutdown signal received"
-	sleep 1
-	kill $pid
-	exit
-}
-
 FACTORIO_STARTUP_COMMANDS=""
 if [ ! -z ${FACTORIO_NO_AUTO_PAUSE+x} ]; then
 	if [ "$FACTORIO_NO_AUTO_PAUSE" = "1" ]; then
 		FACTORIO_STARTUP_COMMANDS="$FACTORIO_STARTUP_COMMANDS --no-auto-pause"
 	fi
 fi
+
+# Check for autosave file
+echo "Checking autosaves.."
+cd /factorio/saves && ./check_autosave.sh
+
 # Run the server
 echo "Starting Factorio.."
-./factorio/bin/x64/factorio --start-server "$FACTORIO_WORLD_NAME".zip --autosave-interval "$FACTORIO_AUTOSAVE_INTERVAL" --latency-ms "$FACTORIO_LATENCY_MS" $FACTORIO_STARTUP_COMMANDS &
-pid="$!"
-
-wait
+cd /
+./factorio/bin/x64/factorio --start-server "$FACTORIO_WORLD_NAME".zip --autosave-interval "$FACTORIO_AUTOSAVE_INTERVAL" --latency-ms "$FACTORIO_LATENCY_MS" $FACTORIO_STARTUP_COMMANDS

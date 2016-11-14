a827ee7a270574c08ef8fdd2ab3dbca76be43d4a
Pauli Jokela <pauli.jokela@didstopia.com>
2016-11-14 12:32:23 +0200

    Pushing changes

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Set Docker to use the machine
-eval "$(docker-machine env default)"
-
 docker tag -f didstopia/factorio-server:latest didstopia/factorio-server:latest
 docker push didstopia/factorio-server:latest

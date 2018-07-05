cd9b41688a1ed1da3102a290793b584c52fca7bf
Pauli Jokela <pauli.jokela@didstopia.com>
2018-07-05 12:43:56 +0300

    Pushing changes [skip ci]

--- docker_run.sh
+++ docker_run.sh
@@ -3,5 +3,5 @@
 ./docker_build.sh
 
 # Run the server
-docker run -p 34197:34197/udp -v $(pwd)/factorio_data:/factorio --name factorio-server -d didstopia/factorio-server:latest
-docker logs -f factorio-server
+docker run -p 34197:34197/udp -v $(pwd)/factorio_data:/factorio --name factorio-server --rm -it didstopia/factorio-server:latest
+#docker logs -f factorio-server
